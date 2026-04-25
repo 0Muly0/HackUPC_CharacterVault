cf7f47a3caa81132120814d0075dc3a9107d44d1
0Muly0 <lucia.famoso@mail.polimi.it>
2026-04-25 18:56:39 +0200

    Added stats and skills page

--- character-vault/src/app/character-sheet/handbooks/the-witcher/tw-overlay/tw-overlay.ts
+++ character-vault/src/app/character-sheet/handbooks/the-witcher/tw-overlay/tw-overlay.ts
@@ -4,6 +4,8 @@ import { geralt } from '../ch_witcher.premade';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RACES } from '../config/races.config';
 import { Perk } from '../models/race.model';
+import { BASE_STATS } from '../config/stats.config';
+import { BASE_SKILLS } from '../config/skills.config';
 
 @Component({
   selector: 'app-tw-overlay',
@@ -30,6 +32,9 @@ export class TwOverlay {
   ];
 
   public character: ChWitcher;
+  public baseStatsArray: [string, number][] = [];
+  public derivStatsArray: [string, number][] = [];  
+  public baseSkillsArray: any[] = [];
 
   public infoForm = new FormGroup({
     name: new FormControl('', Validators.required),
@@ -44,6 +49,7 @@ export class TwOverlay {
   constructor() {
     this.character = geralt;
     this.initInfoForm();
+    this.initSkillStatForm();
   }
 
   changeActiveView(newview: 'info'|'stsk'|'prof') {
@@ -70,6 +76,49 @@ export class TwOverlay {
     }
   }
 
+  private initSkillStatForm() {
+    if(this.character) {
+      this.baseStatsArray = Object.entries(this.character.baseStatSet);
+      this.derivStatsArray = Object.entries(this.character.derivStatSet);
+
+      let skillsByStat = [];
+      // For each stat
+      for(let statKey in BASE_STATS) {
+        // Extract stat level
+        const statLvl = this.character.baseStatSet[statKey as keyof typeof BASE_STATS];
+
+        // For each ability
+        const abilities: any[] = [];
+        Object.keys(BASE_SKILLS).forEach((skillKey) => {
+          // Recovers the ability info
+          const abilitydata = BASE_SKILLS[skillKey as keyof typeof BASE_SKILLS];
+          // If the ability is related to the specified stat...
+          if(abilitydata.stat === statKey) {
+            const skillLvl = this.character.baseSkillSet[skillKey as keyof typeof BASE_SKILLS];
+
+            abilities.push({
+              skillKey: skillKey,
+              skillLvl: skillLvl,
+              label: abilitydata.label,
+              description: abilitydata.description,
+              weight: abilitydata.weight
+            })
+          }
+        })
+
+        if(abilities.length) {
+          skillsByStat.push({
+            statKey: statKey,
+            statLvl: statLvl,
+            label: BASE_STATS[statKey as keyof typeof BASE_STATS].label,
+            abilities: abilities
+          })
+        }
+      }
+      this.baseSkillsArray = skillsByStat;
+    }
+  }
+
   public getRaceDescription(): string {
     if(this.character.race) {
       return RACES[this.character.race].description;
@@ -84,5 +133,9 @@ export class TwOverlay {
     } else {
       return [];
     }
+  }
+
+  public getCharacterBaseStats() {
+    return Object.entries(this.character.baseStatSet);
   }
 }
